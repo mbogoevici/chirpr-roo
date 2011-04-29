a9f3c2b6a062e16add44247a3144c97fe20fd9ad
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-04-29 15:55:49 -0400

    Fix label issues, add converter

--- src/main/java/org/jboss/chirpr/roo/web/ApplicationConversionServiceFactoryBean.java
+++ src/main/java/org/jboss/chirpr/roo/web/ApplicationConversionServiceFactoryBean.java
@@ -1,5 +1,9 @@
 package org.jboss.chirpr.roo.web;
 
+import java.util.Set;
+
+import org.jboss.chirpr.roo.domain.Profile;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
 import org.springframework.roo.addon.web.mvc.controller.RooConversionService;
@@ -13,7 +17,31 @@ public class ApplicationConversionServiceFactoryBean extends FormattingConversio
 	@Override
 	protected void installFormatters(FormatterRegistry registry) {
 		super.installFormatters(registry);
-		// Register application converters and formatters
+	    registry.addConverter(getProfileSetConverter());
 	}
+	
+	private Converter<Set<Profile>, String> getProfileSetConverter() {
+        return new Converter<Set<Profile>, String>() {
+            public String convert(Set<Profile> profiles) {
+            	StringBuilder builder = new StringBuilder();
+                for (Profile profile:profiles) {
+                	if (builder.length()!=0) {
+                		builder.append(",");
+                	}
+                	builder.append(profile.getUsername());	                	
+                }
+                return builder.toString();
+            }
+        };
+    }
+	
+	private Converter<Profile, String> getProfileConverter() {
+        return new Converter<Profile, String>() {
+            public String convert(Profile profile) {
+            	return profile.getUsername();
+            }
+        };
+    }
+
 	
 }
